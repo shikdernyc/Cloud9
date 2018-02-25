c5ba546052518364ba2f0c2de47df17693d5d8ea
Asif Shikder <32821894+shikdernyc@users.noreply.github.com>
2018-02-25 14:12:00 -0500

    Forecast now is able to populate the current weather. A emitter is created to notify when new date is received

--- Forecast.js
+++ Forecast.js
@@ -1,26 +1,48 @@
-function Forecast(api,latitude,longitude)
-{
-    this.api = api;
-    this.latitude = latitude;
-    this.longitude = longitude;
+const Emitter = require('events');
+const https = require('https');
 
-    Forecast.prototype.getWeatherData = function(){
-        console.log("Getting Weather Data");
-        const https = require('https');
-        let link = 'https://api.darksky.net/forecast/' + api + '/' + latitude + ',' + longitude;
+class Forecast {
+    constructor(api) {
+        this.api = api;
+        this.current = {};
+        this.updater = new Emitter();
+    }
+
+    //Get's weather data from a certain location
+    getWeatherData(latitude, longitude) {
+        // console.log("Getting Weather Data");
+        let link = 'https://api.darksky.net/forecast/' + this.api + '/' + latitude + ',' + longitude;
         let JSONData = "";
 
-        https.get(link,res=>{
-          res.on('data',data =>{
-              JSONData += data;
-          });
-          res.on('end',() =>
-          {
-              JSONData = JSON.parse(JSONData);
-              console.dir(JSONData);
-          })
+        https.get(link, res => {
+            res.on('data', data => {
+                JSONData += data;
+            });
+            res.on('end', () => {
+                JSONData = JSON.parse(JSONData);
+                // console.dir(JSONData);
+                this.current = this.updateCurrently(JSONData.currently);
+                this.updater.emit("update");
+            })
         })
-    };
+    }
 
-    module.exports.Forecast = Forecast;
+    //Update Current Data
+    updateCurrently(JSONCurrently)
+    {
+        return{
+            summary: JSONCurrently.summary,
+            icon: JSONCurrently.icon,
+            temperature: JSONCurrently.temperature,
+            apparentTemperature: JSONCurrently.apparentTemperature,
+            humidity: JSONCurrently.humidity,
+            pressure: JSONCurrently.pressure,
+            windSpeed: JSONCurrently.windSpeed,
+            uvIndex: JSONCurrently.uvIndex,
+            visibility: JSONCurrently.uvIndex
+        }
+    }
+
 }
+
+module.exports = Forecast
