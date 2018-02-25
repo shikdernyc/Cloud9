dee63f5983ecd7ed639581f14bacb86130596542
Asif Shikder <32821894+shikdernyc@users.noreply.github.com>
2018-02-25 14:12:34 -0500

    Added code to listen to update event from Forecast

--- app.js
+++ app.js
@@ -1,10 +1,17 @@
 const express = require('express');
-const app = express();
-app.listen(80);
+// const app = express();
+const Forecast = require('./Forecast');
+
+// app.listen(80);
 
 api = 'f7cd012e7d08b33dab0a475e9c0585e9';
+// new Forecast(api)
 latitude = 37.8267;
 longitude = -122.4233;
+let weather = new Forecast(api);
+weather.getWeatherData(latitude, longitude);
 
-const forecast = new Forecast(api,latitude,longitude);
-forecast.getWeatherData();
+weather.updater.on("update" , ()=>{
+    console.log("Event Occured")
+    console.dir(weather.current);
+});
