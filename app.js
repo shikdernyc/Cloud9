2d612d9f9221bf795c5471f6fa4f58f3add7ed97
Asif Shikder <32821894+shikdernyc@users.noreply.github.com>
2018-03-02 20:29:00 -0500

    Allowing Express Updating pug HTML

--- app.js
+++ app.js
@@ -1,19 +1,35 @@
 const express = require('express');
-// const app = express();
-const Forecast = require('./Forecast');
-require('dotenv').config()
+const app = express();
+require('dotenv').config();
+const Forecast = require('./data/Forecast');
 
+const port = 3000;
 
-// app.listen(80);
+app.listen(port);
+
+console.log("Server Running on: localhost:" + port);
+
+const htmlUpdater = function(res, currently, hours, days)
+{
+    res.render('index.pug', {currently, hours, days});
+};
 
 api = process.env.api;
 latitude = 37.8267;
 longitude = -122.4233;
 let weather = new Forecast(api);
 
-weather.getWeatherData(latitude, longitude);
-
 weather.updater.on("update" , ()=>{
-    console.log("Event Occured")
-    console.log(weather.current);
+    console.dir(weather.current);
+    console.dir(weather.hourly);
+    console.dir(weather.daily);
+
+});
+
+
+app.get('/',(req, res) =>{
+    weather.getWeatherData(latitude, longitude);
+    weather.updater.on("update", ()=>{
+        htmlUpdater(res, weather.current, weather.hourly, weather.daily);
+    })
 });
