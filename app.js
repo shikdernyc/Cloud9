2d0153fef672d44f71834f9cbda57340782b97d1
Asif Shikder <32821894+shikdernyc@users.noreply.github.com>
2018-03-06 00:39:09 -0500

    Refactoring of codes to make things cleaner

--- app.js
+++ app.js
@@ -1,35 +1,40 @@
+require('dotenv').config();
 const express = require('express');
 const app = express();
-require('dotenv').config();
-const Forecast = require('./data/Forecast');
+const Forecast = require('./src/Forecast');
+const router = require('./src/Router');
 
-const port = 80;
-
+//Starting Server
+const port = 3000;
 app.listen(port);
+console.log("Server Running on: localhost:" + port);
 
-// console.log("Server Running on: localhost:" + port);
+//Populating Data from .env files
+geocodingAPI = process.env.GEO_API;
+forecastAPI = process.env.FORECAST_API;
 
-const htmlUpdater = function(res, currently, hours, days)
+//Setting up routing
+app.use(router.router);
+
+//Listening to when router get's a request from user about weather from a location
+router.event.on("get-forecast",(res,latitude,longitude)=>{
+    weather.getWeatherData(latitude,longitude, (current, hourly, daily)=>{
+        updateHTML(res, current, hourly, daily);
+    });
+});
+
+// latitude = 37.8267;
+// longitude = -122.4233;
+let weather = new Forecast(forecastAPI);
+
+
+const updateHTML = function(res, currently, hours, days)
 {
-    res.render('index.pug', {currently, hours, days});
+    res.statusCode = 200;
+    res.contentType('text/html');
+    console.dir({currently, hours});
+    res.locals.hours = hours;
+    res.locals.days = days;
+    res.render('index.pug', currently);
+    res.end();
 };
-
-api = process.env.api;
-latitude = 37.8267;
-longitude = -122.4233;
-let weather = new Forecast(api);
-
-weather.updater.on("update" , ()=>{
-    console.dir(weather.current);
-    console.dir(weather.hourly);
-    console.dir(weather.daily);
-
-});
-
-
-app.get('/',(req, res) =>{
-    weather.getWeatherData(latitude, longitude);
-    weather.updater.on("update", ()=>{
-        htmlUpdater(res, weather.current, weather.hourly, weather.daily);
-    })
-});
