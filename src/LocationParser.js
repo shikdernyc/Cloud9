88eb8f8249e609de6d37785e50c21a41cb5407d5
Asif Shikder <32821894+shikdernyc@users.noreply.github.com>
2018-03-06 13:37:43 -0500

    Location Parser able to get GeoCoded location from google API

--- src/LocationParser.js
+++ src/LocationParser.js
@@ -6,27 +6,43 @@ class LocationParser{
     {
         this.api = api;
     }
-
     /**
      * Get's JSON Data from a certain address
      * @param address - Location's Address
      */
-    getJSONData (address) {
-        let link = url + "json?address=" + address + "&" + "key=" + api;
+    getJSONData (address,callback) {
+        let link = url + "json?address=" + address + "&" + "key=" + this.api;
         https.get(link, res => {
             let JSONData = "";
             res.on('data', data => {
                 JSONData += data;
             });
 
-            res.on('end', () => {
+            res.on('end', (data) => {
+                // JSONData += data;
                 JSONData = JSON.parse(JSONData);
-                console.dir(JSONData)
+                callback(JSONData.status, JSONData.results);
             })
         })
     }
+
+    getCodedLocation(address, callback){
+        this.getJSONData(address, (status, results)=>{
+            if(status === 'OK')
+            {
+                callback(results[0].geometry.location);
+            }
+            else
+            {
+                callback(null);
+            }
+        });
+    }
 }
 
-// let address='1600+Amphitheatre+Parkway,+Mountain+View,+CA';
-// getJSONData(address);
-console.log(api);
+let lp = new LocationParser('AIzaSyCBLWfBy_il1velXGorewJXfriEsQ0grbc');
+let address='1600+Amphitheatre+Parkway,+Mountain+View,+CA';
+
+lp.getCodedLocation(address, (location)=>{
+    console.dir(location);
+})
