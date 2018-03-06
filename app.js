d5921f455eace92195cbb159330581a62908c6fa
Asif Shikder <32821894+shikdernyc@users.noreply.github.com>
2018-03-06 14:15:15 -0500

    Added Location Parser to handle any addresses

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ require('dotenv').config();
 const express = require('express');
 const app = express();
 const Forecast = require('./src/Forecast');
+const LocationParser = require('./src/LocationParser');
 const router = require('./src/Router');
 
 //Starting Server
@@ -13,19 +14,24 @@ console.log("Server Running on: localhost:" + port);
 forecastAPI = process.env.FORECAST_API;
 geocodingAPI = process.env.GEO_API;
 
+
+let weather = new Forecast(forecastAPI);
+let lp = new LocationParser(geocodingAPI);
+
 //Setting up routing
 app.use(router.router);
 
 //Listening to when router get's a request from user about weather from a location
-router.event.on("get-forecast",(res,latitude,longitude)=>{
-    weather.getWeatherData(latitude,longitude, (current, hourly, daily)=>{
-        updateHTML(res, current, hourly, daily);
+router.event.on("get-forecast",(res,location)=>{
+    //Asks the Location Parser to return latitude and longitude values
+    lp.getCodedLocation(location, (address)=>{
+        //Get's weather data for location
+        weather.getWeatherData(address.lat,address.lng, (current, hourly, daily)=>{
+            //Updates Weather data
+            updateHTML(res, current, hourly, daily);
+        });
     });
 });
-
-// latitude = 37.8267;
-// longitude = -122.4233;
-let weather = new Forecast(forecastAPI);
 
 
 //Update weather display
