8784c714e039c2dc31a9f0aaf745435f0f8a98cd
Asif Shikder <32821894+shikdernyc@users.noreply.github.com>
2018-03-06 14:14:12 -0500

    Refactoring of getting location

--- src/Router.js
+++ src/Router.js
@@ -4,19 +4,17 @@ const event = new eventEmitter();
 const router = express.Router();
 
 //Home Route
-router.route('/').get((req, res)=> {
+router.route('/').get((req, res) => {
     // res.send("Landing Page");
     res.render('index.pug', {});
 
 });
 
+
 //TODO: Get request using Google GEO API
 //Requesting a location
-router.route('/location').get((req, res)=>{
-    url = req.url;
-    latitude = parseFloat(url.substring(url.indexOf('latitude=') + 9, url.indexOf('&')));
-    longitude = parseFloat(url.substring(url.indexOf('longitude=') + 10));
-    event.emit('get-forecast', res, latitude, longitude);
+router.route('/location').get((req, res) => {
+    event.emit('get-forecast', res, req.url.substring(req.url.indexOf('address=') + 8,req.url.length)); //Parses the address
 });
 
-module.exports = {router,event};
+module.exports = {router, event};
