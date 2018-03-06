3d8a941b7e6ff69576211c6171d8f20f0b26bded
Asif Shikder <32821894+shikdernyc@users.noreply.github.com>
2018-03-06 00:35:49 -0500

    Changed getForecast to a Callback function Updated ForecastParser request to the new parameters for hourly and daily

--- src/Forecast.js
+++ src/Forecast.js
@@ -1,36 +1,38 @@
 const https = require('https');
 const forecastParser = require('./ForecastParser.js');
-const Emitter = require('events');
 class Forecast {
     constructor(api) {
-        this.api = process.env.api;
-        this.updater = new Emitter();
+        this.api = api;
         this.current = {};
         this.hourly = [];
         this.daily = [];
         this.timeZone="";
     }
 
-    //Get's weather data from a certain location
-    getWeatherData(latitude, longitude) {
+    //Get's weather src from a certain location
+    //callback function returns the current data, hourly data and daily data
+    getWeatherData(latitude, longitude, callback) {
         let link = 'https://api.darksky.net/forecast/' + this.api + '/' + latitude + ',' + longitude;
         let JSONData = "";
 
         https.get(link, res => {
+            console.log(res.statusCode);
             res.on('data', data => {
                 JSONData += data;
             });
+
+            //Returns all the data in a callback function at the end
             res.on('end', () => {
                 JSONData = JSON.parse(JSONData);
                 console.log("JSONData Received from Forecast.io")
                 this.timeZone = JSONData.timezone;
                 this.current = forecastParser.currentData(JSONData.currently);
-                this.hourly = forecastParser.hourlyData(JSONData.hourly);
-                this.daily = forecastParser.dailyData(JSONData.daily);
-                this.updater.emit("update");
+                this.hourly = forecastParser.hourlyData(JSONData.hourly, 24);
+                this.daily = forecastParser.dailyData(JSONData.daily, 7);
+                callback(this.current, this.hourly, this.daily);
             })
         })
     }
 }
 
-module.exports = Forecast
+module.exports = Forecast;
